eac0f891a763f8354f2a97fefa6e9fb93c2e3113
rahul-1729 <rk9763981@gmail.com>
2025-01-13 20:00:20 +0000

    tokenizer done

--- Tokenizer/simple_tokenizer_v1.py
+++ Tokenizer/simple_tokenizer_v1.py
@@ -7,7 +7,11 @@ class simple_tokenizer_v1:
     def encode(self,sentence):
         preprocessed = re.split(r'([,.?_!"()\']|--|\s)',sentence)
         tokens = [ token.strip() for token in preprocessed if token.strip()]
-        ids = [self]
+        token_ids = [self.str_to_int[token] for token in tokens]
+        return token_ids
 
     def decode(self,token_ids):
-        pass
+        sentence = " ".join([self.int_to_str(token_id) for token_id in token_ids])
+        sentence = re.sub(r'\s+([,.?!"()\'])', r'\1',sentence) 
+        return sentence
+        
